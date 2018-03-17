dab7fcaccd280c34c2573b91fbe17274665b5a0a
1ven <1ven.mironchik@gmail.com>
2018-03-17 14:33:08 +0300

    Rename user controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,7 @@
 import Router from "koa-router";
-import users from "./users";
+import user from "./user";
 import auth from "./auth";
 
 export default new Router({ prefix: "/api/v1" })
-  .use(users.routes(), auth.routes())
+  .use(user.routes(), auth.routes())
   .routes();
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,7 +2,7 @@ import Router from "koa-router";
 import { authenticated } from "core/libraries/koa/middlewares";
 import * as model from "models/user";
 
-export default new Router({ prefix: "/users" })
+export default new Router({ prefix: "/user" })
   .get("/", authenticated, async ctx => {
     ctx.response.body = await model.readById(ctx.session.userId, ctx.db);
   })
