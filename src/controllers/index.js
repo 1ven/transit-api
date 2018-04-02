078c1dc0849a5a083379f814a09e0e7f084282b4
1ven <1ven.mironchik@gmail.com>
2018-04-02 21:24:27 +0300

    Rename user controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,8 +2,8 @@ import Router from "koa-router";
 import auth from "./auth";
 import credentials from "./credentials";
 import self from "./self";
-import user from "./user";
+import users from "./users";
 
 export default new Router({ prefix: "/v1" })
-  .use(auth.routes(), credentials.routes(), self.routes(), user.routes())
+  .use(auth.routes(), credentials.routes(), self.routes(), users.routes())
   .routes();
